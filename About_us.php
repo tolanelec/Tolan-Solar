e7b1b377c4380c2fd29f757e7fe3de4459582a37
lanre <larry@tolanelec.co.uk>
2013-10-23 19:59:58 +0100

    updated again

---
+++ About_us.php
@@ -0,0 +1,231 @@
+<?php if(preg_match("/MSIE/",getenv("HTTP_USER_AGENT")) || preg_match("/Internet Explorer/",getenv("HTTP_USER_AGENT"))){header('Location:http://localhost/xampp/tolansolar-v1/About_us.php'); }?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transistional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+
+    <title>Tolan Solar | About tolan| </title>
+<link rel="stylesheet" href="default.css" type="text/css" />	
+
+ <link rel="stylesheet" href="main3_style.css" type="text/css" />
+ <link rel="stylesheet" type="text/css" href="bootstrap.css" />
+
+
+
+
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js">
+
+</script>
+
+<script type-"text/javascript" src="jquerybrowserdetect.js">
+</script>
+
+<script type="text/javascript" src="jquery.js"></script>
+<script type="text/javascript">
+$(document).ready(function(){
+  $("p.text").click(function(){
+    $(this).hide();
+  });
+});
+</script>
+<meta name="description" content=" for affordable solar and lighting products "/>
+<meta name="keywords" content="About Tolan","LED-Lighting production", "Lighting products ", "Solar company"  />
+</head>
+<body>
+
+<div>
+<script>
+  (function() {
+    var cx = '016733281910472351058:azkatytop1i';
+    var gcse = document.createElement('script'); gcse.type = 'text/javascript'; gcse.async = true;
+    gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+        '//www.google.com/cse/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(gcse, s);
+  })();
+</script>
+
+   <div id="Header">
+	     <div id="menu"> 
+		    <ul class="topmenu">
+			  <li class="search">
+			 <gcse:searchbox-only></gcse:llsearchbox-only>
+              </li>
+			</ul>
+			
+ 		 </div>
+		 
+		 <div id="logo">
+		   <span id="imag">
+			<img src="images/Tolan-Logo.jpg" alt="tolansolar logo"/>
+		   </span>
+		 </div>
+	
+	
+		 <ul class="navigate">
+		    <li><a class="nav" href="home.php"> Home </a>
+				<ul>
+					<li><a href="About_us.php">Who we are</a></li>
+					<li><a href="Charity.php">Community Support</a></li>
+					<li><a href="Case_study.php">Case Study</a></li>
+				</ul>
+			</li>			
+			<li><a class="nav" href="Contact.php"> Contact  </a></li>
+			<li><a class="nav" href="Products.php"> Products</a>
+				<ul>
+					<li><a href="House.php">Home Products </a></li>
+					<li><a href="Lighting.php">Lighting Products</a></li>
+				
+				</ul>
+			</li>
+			<li><a class="nav" href="PV-Installation.php">Installation</a>
+
+			</li>
+			<li><a class="nav" href="Support.php">Support</a>
+				<ul>
+					<li><a href="Frequent_Questions.php">FAQ </a></li>
+					<li><a href="Repairs.php">Repairs</a></li>
+					<li><a href="Manuals.php">Manuals</a></li>
+					<li><a href="Deliveries.php">Deliveries</a></li>
+				</ul>
+			</li>
+		 </ul>
+	</div>
+	
+    <div id="cf">
+	  	   <img class="bottom" src="images/container_bg9.png" alt="sunset off dock"/>
+           <img class="top" src="images/container_bg3.jpg" alt="solar panel"/>	
+	</div>
+	
+	<div id="sidebar">
+	    <div id="column1">
+		<div id="column_left">
+			<span id="contentsfaq_update">
+		
+	        <h3 class="grey">About Tolan </h3>
+			<p>
+				<ul class="sidemenu">
+					<li><a href="About_us1.php">Our business </a></li>
+					<li><a href="About_us2.php">Research </a></li>
+					<li><a href="About_us2.php#business">Business Philosophy </a></li>
+					<li><a href="About_us3.php">Projects</a> </LI>
+
+				</UL>
+			</p>	
+			    </br>
+				</br>
+				<h3 class="grey_bottom">Tolan Sections </h3>
+				<UL class="sidemenu">
+					<li><a href="Branches.php">Offices</a></li>
+					<li><a href="Contact.php">Contact</a></li>
+					<li><a href="Departments.php">Departments</a></li>
+					<li><a href="Training.php">Training</a></li>
+					<li><a href="Staff.php">Staff Login </a></li>
+				 
+				 </ul>
+			</span>
+			</div>
+			
+			
+		<div id="column_small">	
+			
+			<span id="topic">
+				<h3 id="topic">ABOUT US </h3>
+			</span>
+			<span id="contents4">
+			 <h3 class="blue">TOLAN SOLAR : OUR COMPANY </h3>
+		
+			<blockquote>
+			<p class="text">
+				Our business aims to provide cheaper alternatives to carbon-fuelled energy creation, by utilizing
+				existing and new technologies to cater for the domestic market. We believe in the potential
+				benefit of research in areas of ElectroChemistry, Electronics and <abbr title="photo voltaic ">PV</abbr>-Technology to produce Solutions
+				for unique problems in third world countries.
+			</p>
+			<p class="text">
+			 The Nigerian market: 
+				With the epileptic nature of electricity in Nigeria. Tolan has focused its attention on providing unique solutions that can 
+				fill the gap left by power shortage and unaffordable infrastructure. 
+				
+			</p>
+
+			</blockquote>
+			</span>
+			<span id="contents4">
+			 <h3 class="blue">
+					 OUR BUSINESS</h3>
+			
+			<blockquote>
+				<p class="text">
+			   Tolan Electronics was set up to provide cheaper and efficient energy and lighting solutions.
+			 Our research has enabled us utilize existing technologies to develop 
+			  products aimed at improving the living standards, reduce energy wastage and provide consumers with quality lighting and electrical products at affordable prices.
+				
+				
+				At the moment we have focused on manufacturing and sales of Energy efficient lighting products and <abbr title="photo voltaic ">PV</abbr>-solar technology. 
+				We also sell electronic parts, semi-conductor resales and made to order designs of lighting equipment.
+				<p class="text">
+				We strive to make new technologies available to the our target market at affordable prices.
+				
+				</p>
+			</blockquote></span>
+			
+</div>
+			
+			<div id="column_right">
+			
+		    <span id="span#contentsfaq_right">
+			
+			<form class="quoter" method="post" action="getquote.php" >
+				<fieldset class ="left">
+                     <h3> GET A QUOTE NOW  </h3>
+					
+					    <label for="namefield">Name</label></br>
+						<input type="textfield"  name='namefield' value=""/></br>
+						<label for="email">Email</label></br>
+						<input type="textfield" name='emailfield' value="" /></br>
+					
+						<label for="phone">contact number</label></br>
+						<input type="textfield" name="contactfield" value=""/></br>
+						
+						
+						<label for="product">select product </label></br>
+						<select name="products">
+						  <option>solar array</option>
+						  <option>roof installation</option>
+						  <option>single room </option>
+						  <option>double room</option>
+						  <option>commercial install</option>
+				        </select></br></br>
+					
+					    <input type="hidden" name="stage" value="process" />
+						<input type="submit" name="submit" value=" Submit Quote" />
+						
+						
+						
+				</fieldset>		
+				</form>	
+			
+			  <span id="contents5">
+
+			</span>	
+				
+
+			
+
+			</span>
+			</div>
+			</div>
+			
+
+	</div>
+  	<div id="footer">
+	<span id="disclaimer" ><p>  This website is copyright of tolansolar limited 2012 (c).Tolan Solar is Uk Registered company.     Please visit our facebook page here  </p>
+	<p>| <a href="Copyright.php">Copy right statement </a>| <a href="Cookie_Privacy.php">Cookies Policy</a> | <a href="Credit.php">Credits</a> | <a href="Privacy.php">Data Privacy Statement</a> | <a href="Conditions.php">Terms and Conditions</a> | 
+	</p> </span>
+	</div>
+
+
+	
+</body>
+</html>
